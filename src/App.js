ae7c82153011001133b67f85b23964a9db163146
Treadaa <treadaaofficial@gmail.com>
2021-07-28 11:29:24 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+
+import home from "home/home.js";
+
+
+import ComponentRenderer from "ComponentRenderer.js";
+import MainLandingPage from "MainLandingPage.js";
+import ThankYouPage from "ThankYouPage.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+export default home;
+
